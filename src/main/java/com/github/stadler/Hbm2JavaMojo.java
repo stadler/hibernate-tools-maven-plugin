4f8d7af10b012bab14fe662f5ded9a43bbd7c104
Jacques Stadler <jacques.stadler@gmail.com>
2017-12-11 22:01:11 +0100

    Refactor to support other exporters

--- src/main/java/com/github/stadler/Hbm2JavaMojo.java
+++ src/main/java/com/github/stadler/Hbm2JavaMojo.java
@@ -1,15 +1,8 @@
 package com.github.stadler;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.hibernate.cfg.JDBCMetaDataConfiguration;
-import org.hibernate.cfg.reveng.DefaultReverseEngineeringStrategy;
-import org.hibernate.cfg.reveng.OverrideRepository;
-import org.hibernate.cfg.reveng.ReverseEngineeringSettings;
-import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2x.POJOExporter;
 
 import java.io.File;
@@ -20,26 +13,10 @@ import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_SOURC
  * Mojo to generate Java JPA Entities from an existing database.
  */
 @Mojo(name = "hbm2java", defaultPhase = GENERATE_SOURCES)
-public class Hbm2JavaMojo extends AbstractMojo {
+public class Hbm2JavaMojo extends AbstractHbm2xMojo {
 
-    @Parameter
-    private String packageName;
     @Parameter(defaultValue = "${project.build.directory}/generated-sources/")
     private File outputDirectory;
-    @Parameter(defaultValue = "${project.basedir}/src/main/hibernate/hibernate.cfg.xml")
-    private File configFile;
-    @Parameter
-    private File revengFile;
-    @Parameter(defaultValue = "true")
-    private boolean detectManyToMany;
-    @Parameter(defaultValue = "true")
-    private boolean detectOneToOne;
-    @Parameter(defaultValue = "true")
-    private boolean detectOptimisticLock;
-    @Parameter(defaultValue = "true")
-    private boolean createCollectionForForeignKey;
-    @Parameter(defaultValue = "true")
-    private boolean createManyToOneForForeignKey;
     @Parameter(defaultValue = "false")
     private boolean ejb3;
     @Parameter(defaultValue = "false")
@@ -47,38 +24,7 @@ public class Hbm2JavaMojo extends AbstractMojo {
     @Parameter
     private String templatePath;
 
-
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Starting " + this.getClass().getSimpleName() + "...");
-
-        ReverseEngineeringStrategy strategy = new DefaultReverseEngineeringStrategy();
-
-        if (revengFile != null) {
-            OverrideRepository override = new OverrideRepository();
-            override.addFile(revengFile);
-            strategy = override.getReverseEngineeringStrategy(strategy);
-        }
-
-        ReverseEngineeringSettings settings =
-                new ReverseEngineeringSettings(strategy)
-                        .setDefaultPackageName(packageName)
-                        .setDetectManyToMany(detectManyToMany)
-                        .setDetectOneToOne(detectOneToOne)
-                        .setDetectOptimisticLock(detectOptimisticLock)
-                        .setCreateCollectionForForeignKey(createCollectionForForeignKey)
-                        .setCreateManyToOneForForeignKey(createManyToOneForForeignKey);
-
-        strategy.setSettings(settings);
-
-        JDBCMetaDataConfiguration cfg = new JDBCMetaDataConfiguration();
-        cfg.setReverseEngineeringStrategy(strategy);
-
-        getLog().info("Configuring using file: " + configFile + "...");
-        cfg.configure(configFile);
-
-        getLog().info("Reading from JDBC...");
-        cfg.readFromJDBC();
-
+    protected void executeExporter(Configuration cfg) {
         POJOExporter pojoExporter = new POJOExporter(cfg, outputDirectory);
         if (templatePath != null) {
             getLog().info("Setting template path to: " + templatePath);
@@ -88,6 +34,7 @@ public class Hbm2JavaMojo extends AbstractMojo {
         pojoExporter.getProperties().setProperty("jdk5", String.valueOf(jdk5));
         getLog().info("Starting POJO export to directory: " + outputDirectory + "...");
         pojoExporter.start();
-        getLog().info("Finished " + this.getClass().getSimpleName() + "!");
     }
+
+
 }
